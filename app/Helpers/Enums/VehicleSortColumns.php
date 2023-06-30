7dabcc88010be03e56a1ed0bbbb4693837fe6e44
Artem Rubets <artemrubets27@gmail.com>
2023-07-01 01:37:40 +0300

    Get a list of vehicles documentation

--- app/Helpers/Enums/VehicleSortColumns.php
+++ app/Helpers/Enums/VehicleSortColumns.php
@@ -7,4 +7,10 @@ enum VehicleSortColumns: string
     case brand = 'brand';
     case model = 'model';
     case year = 'year';
+    case state_number = 'state_number';
+    case vin_code = 'vin_code';
+    case name = 'name';
+    case color = 'color';
+    case created_at = 'created_at';
+    case updated_at = 'updated_at';
 }
--- app/Virtual/API/v1/Controllers/VehiclesController.php
+++ app/Virtual/API/v1/Controllers/VehiclesController.php
@@ -41,7 +41,7 @@ use OpenApi\Annotations as OA;
  *         )
  *     ),
  *     @OA\Response(
- *          response=200,
+ *          response=201,
  *          description="Successful operation",
  *          @OA\JsonContent(
  *              @OA\Property(property="data", type="object",
@@ -49,6 +49,164 @@ use OpenApi\Annotations as OA;
  *              ),
  *          )
  *      )
+ * )
+ *
+ *
+ * @OA\Get(
+ *      path="/api/v1/vehicles/",
+ *      tags={"Vehicles"},
+ *      summary="Get a list of vehicles",
+ *      description="Get a list of vehicles",
+ *      @OA\Parameter(
+ *          name="brand",
+ *          description="Filter by Brand",
+ *          required=false,
+ *          example="Ford",
+ *          in="query",
+ *          @OA\Schema(
+ *              type="string"
+ *          )
+ *      ),
+ *      @OA\Parameter(
+ *           name="model",
+ *           description="Filter by Model",
+ *           required=false,
+ *           example="Fusion",
+ *           in="query",
+ *           @OA\Schema(
+ *               type="string"
+ *           )
+ *      ),
+ *      @OA\Parameter(
+ *           name="year",
+ *           description="Filter by Model year",
+ *           required=false,
+ *           example=2017,
+ *           in="query",
+ *           @OA\Schema(
+ *               type="integer"
+ *           )
+ *      ),
+ *      @OA\Parameter(
+ *            name="sort_by",
+ *            description="Colunm for sorting",
+ *            required=false,
+ *            example="year",
+ *            in="query",
+ *            @OA\Schema(
+ *                type="string",
+ *                enum={
+ *                      "brand",
+ *                      "model",
+ *                      "year",
+ *                      "state_number",
+ *                      "vin_code",
+ *                      "name",
+ *                      "color",
+ *                      "created_at",
+ *                      "updated_at"
+ *                }
+ *            )
+ *      ),
+ *      @OA\Parameter(
+ *            name="sort_order",
+ *            description="Sort direction",
+ *            required=false,
+ *            example="desc",
+ *            in="query",
+ *            @OA\Schema(
+ *                type="string",
+ *                enum={
+ *                      "asc",
+ *                      "desc",
+ *                }
+ *            )
+ *      ),
+ *      @OA\Response(
+ *           response=200,
+ *           description="Successful operation",
+ *           @OA\JsonContent(
+ *               @OA\Property(property="data", type="object",
+ *                   ref="#/components/schemas/VehicleResource"
+ *               ),
+ *               @OA\Property(property="links", type="object",
+ *                   @OA\Property(property="first", type="string"),
+ *                   @OA\Property(property="last", type="string"),
+ *                   @OA\Property(property="prev", type="string", nullable=true),
+ *                   @OA\Property(property="next", type="string", nullable=true),
+ *               ),
+ *               @OA\Property(property="meta", type="object",
+ *                   @OA\Property(property="current_page", type="integer"),
+ *                   @OA\Property(property="from", type="integer"),
+ *                   @OA\Property(property="last_page", type="integer"),
+ *                   @OA\Property(property="links", type="array",
+ *                      @OA\Items(
+ *                          @OA\Property(property="url", type="string", nullable=true),
+ *                          @OA\Property(property="label", type="string"),
+ *                          @OA\Property(property="active", type="boolean")
+ *                      )
+ *                   ),
+ *                   @OA\Property(property="path", type="string"),
+ *                   @OA\Property(property="per_page", type="integer"),
+ *                   @OA\Property(property="to", type="integer"),
+ *                   @OA\Property(property="total", type="integer"),
+ *               ),
+ *               example={
+ *                  "data": {
+ *                      {
+ *                          "name": "Brad",
+ *                          "state_number": "BM3452BX",
+ *                          "color": "black",
+ *                          "vin_code": "3FA6P0VP1HR282209",
+ *                          "model": "Fusion",
+ *                          "brand": "FORD",
+ *                          "year": "2017"
+ *                      },
+ *                      {
+ *                          "name": "Brad",
+ *                          "state_number": "BM3X",
+ *                          "color": "black",
+ *                          "vin_code": "3VWDP7AJ7DM356782",
+ *                          "model": "Jetta",
+ *                          "brand": "VOLKSWAGEN",
+ *                          "year": "2013"
+ *                      },
+ *                   },
+ *                   "links": {
+ *                      "first": "http://127.0.0.1/api/v1/vehicles?page=1",
+ *                      "last": "http://127.0.0.1/api/v1/vehicles?page=1",
+ *                      "prev": null,
+ *                      "next": null
+ *                   },
+ *                   "meta": {
+ *                      "current_page": 1,
+ *                      "from": 1,
+ *                      "last_page": 1,
+ *                      "links": {
+ *                          {
+ *                              "url": null,
+ *                              "label": "&laquo; Previous",
+ *                              "active": false
+ *                          },
+ *                          {
+ *                              "url": "http://127.0.0.1/api/v1/vehicles?page=1",
+ *                              "label": "1",
+ *                              "active": true
+ *                          },
+ *                          {
+ *                              "url": null,
+ *                              "label": "Next &raquo;",
+ *                              "active": false
+ *                          }
+ *                      },
+ *                      "path": "http://127.0.0.1/api/v1/vehicles",
+ *                      "per_page": 10,
+ *                      "to": 3,
+ *                      "total": 3
+ *                   }
+ *              }
+ *           )
+ *       )
  * )
  */
 
